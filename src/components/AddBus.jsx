6ce597c73ef18b5894a7919c54f02bebdce03caa
gauthmz <gauthambabu12@gmail.com>
2024-04-11 17:40:31 +0530

    Add Bus modiefied

--- src/components/AddBus.jsx
+++ src/components/AddBus.jsx
@@ -4,7 +4,7 @@ import BusNavbar from './BusNavbar'
 const AddBus = () => {
     return (
         <div>
-            <BusNavbar/>
+            <BusNavbar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -22,16 +22,12 @@ const AddBus = () => {
                                 <input type="text" className="form-control" />
 
                             </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">Route</label>
                                 <input type="text" className="form-control" />
 
                             </div>
-
-                        </div>
-                        <br />
-                        <div className="row g-3">
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Driver Name</label>
