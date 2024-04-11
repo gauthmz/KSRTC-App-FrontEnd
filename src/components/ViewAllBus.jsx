2c50d2231bd7298e5428f583191ce3b28dd1afe8
gauthmz <gauthambabu12@gmail.com>
2024-04-11 17:35:13 +0530

    View all Bus modified"

--- src/components/ViewAllBus.jsx
+++ src/components/ViewAllBus.jsx
@@ -4,43 +4,55 @@ import BusNavbar from './BusNavbar'
 const ViewAllBus = () => {
     return (
         <div>
-            <BusNavbar/>
+            <BusNavbar />
             <br />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <ul class="list-group list-group-horizontal">
-                                    <li class="list-group-item">KSRTC2020S25</li>
-                                    <li class="list-group-item">KL-01-K-206</li>
-                                    <li class="list-group-item">Thrissur-Malappuran</li>
-                                    <li class="list-group-item">Saji TK</li>
-                                    <li class="list-group-item">8506592099</li>
-                                </ul>
-                                <br />
-                                <ul class="list-group list-group-horizontal-sm">
-                                    <li class="list-group-item">KSRTC2021B85</li>
-                                    <li class="list-group-item">KL-01-K-1025</li>
-                                    <li class="list-group-item">Thiruvananthapuram-Kozhikode</li>
-                                    <li class="list-group-item">Rajeev Manoj</li>
-                                    <li class="list-group-item">9854263125</li>
-                                </ul>
-                                <br />
-                                <ul class="list-group list-group-horizontal-md">
-                                    <li class="list-group-item">KSRTC2018Z62</li>
-                                    <li class="list-group-item">KL-01-K-56</li>
-                                    <li class="list-group-item">Ernakulam-Munnar</li>
-                                    <li class="list-group-item">Akhilesh V</li>
-                                    <li class="list-group-item">9747002602</li>
-                                </ul>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">KL-01-K-206</h5>
+                                        <h6 class="card-subtitle mb-2 text-body-secondary">Thrissur-Malappuram</h6>
+                                        <p class="card-text"></p>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">KSRTC2020S25</li>
+                                        <li class="list-group-item">Saji TK, 8506592099</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">KL-01-K-1025</h5>
+                                        <h6 class="card-subtitle mb-2 text-body-secondary">Ernakulam-Munnar</h6>
 
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">KSRTC2021B85</li>
+                                        <li class="list-group-item">Akhilesh V, 9747002602</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">KL-01-K-56</h5>
+                                        <h6 class="card-subtitle mb-2 text-body-secondary">Thiruvananthapuram-Kozhikode</h6>
+
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">KSRTC2018Z62</li>
+                                        <li class="list-group-item">Rajeev Manoj, 9854263125</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
